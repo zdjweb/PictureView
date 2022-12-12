c7ef52bfd3e20d1379bb8210de7e2ac7a92d7e5a
zdjweb <63274181+zdjweb@users.noreply.github.com>
2022-12-13 00:21:08 +0800

    Add files via upload

---
+++ functions/getImage.js
@@ -0,0 +1,28 @@
+export function onRequest(context) {
+    return new Response(`./image/0655c2a84be9269d8d76c77df7f5d08900e52c7d.jpg
+    ./image/0cc250aaf8b7de81a762b346cddd23b52dc69f2c.jpg
+    ./image/19f7b905e0e2394df866f4dc146d20c20a891ebc.jpg
+    ./image/23aa3d56b6a30f8a20d5893e609696fd96beb386.jpg
+    ./image/251ab2a15b32c9fa6b035875b68cf131c941d4fd.jpg
+    ./image/35db33532417f83ff516072a22d6fb4cf33b1b8c.jpg
+    ./image/574e569d44087c75e41a33552a9745d4e07cb12b.jpg
+    ./image/75aa4835c28c43e8d5c88bd4cb9ed9c4538ca7b9.jpg
+    ./image/8f0f01375b5049e0aa604c7eb11caa535a92e0ec.jpg
+    ./image/aa435db0553e61b9367f46a700f2c8a3b35afea9.jpg
+    ./image/d5f0c0bdc80f211b66b9b5831814843c1e51844f.jpg
+    ./image/e1b1a2accf1a91351b16c4a49ec3d95172fc72e1.jpg
+    ./image/ee59c9abbaf8693a6f1637e1e129a763348e86de.jpg
+    ./image/0655c2a84be9269d8d76c77df7f5d08900e52c7d.jpg
+    ./image/0cc250aaf8b7de81a762b346cddd23b52dc69f2c.jpg
+    ./image/19f7b905e0e2394df866f4dc146d20c20a891ebc.jpg
+    ./image/23aa3d56b6a30f8a20d5893e609696fd96beb386.jpg
+    ./image/251ab2a15b32c9fa6b035875b68cf131c941d4fd.jpg
+    ./image/35db33532417f83ff516072a22d6fb4cf33b1b8c.jpg
+    ./image/574e569d44087c75e41a33552a9745d4e07cb12b.jpg
+    ./image/75aa4835c28c43e8d5c88bd4cb9ed9c4538ca7b9.jpg
+    ./image/8f0f01375b5049e0aa604c7eb11caa535a92e0ec.jpg
+    ./image/aa435db0553e61b9367f46a700f2c8a3b35afea9.jpg
+    ./image/d5f0c0bdc80f211b66b9b5831814843c1e51844f.jpg
+    ./image/e1b1a2accf1a91351b16c4a49ec3d95172fc72e1.jpg
+    ./image/ee59c9abbaf8693a6f1637e1e129a763348e86de.jpg`)
+}
